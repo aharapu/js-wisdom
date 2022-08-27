67973902a8e6db09f28878e7578a9d759d401db8
Valle Hara <alexandru.harapu@gmail.com>
2022-08-27 13:35:41 +0300

    Listen to message from pop-up window

--- web-app/src/examples/post-message.js
+++ web-app/src/examples/post-message.js
@@ -1,5 +1,7 @@
 const msgInputElem = document.querySelector("#message-input");
 const sendMsgBtn = document.querySelector("#send-message-button");
+const popupBtn = document.querySelector("#popup-btn");
+const receivedMsg = document.querySelector("#received-msg");
 
 function handleClick() {
   console.log("btn was clicked");
@@ -7,4 +9,21 @@ function handleClick() {
   console.log("current message is: ", message);
 }
 
+function openPopup() {
+  window.open(
+    "http://localhost:3001/examples/post-message-popup.html",
+    "_blank"
+  );
+}
+
 sendMsgBtn.addEventListener("click", handleClick);
+popupBtn.addEventListener("click", openPopup);
+
+window.addEventListener("message", (event) => {
+  console.log("message arrived");
+  receivedMsg.textContent = event.data;
+});
+
+/*
+Note: Setting target="_blank" on <a> elements implicitly provides the same rel behavior as setting rel="noopener" which does not set window.opener. See browser compatibility for support status.
+*/
