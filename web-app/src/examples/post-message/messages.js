f4ca3439b1a68f2296fecd2190efd3479ce844fd
Valle Hara <alexandru.harapu@gmail.com>
2022-09-10 13:47:12 +0300

    Programatically create message section

---
+++ web-app/src/examples/post-message/messages.js
@@ -0,0 +1,14 @@
+export const WINDOW_LOCATION = {
+  popup: "POPUP",
+};
+
+export const MESSAGES = [
+  {
+    id: 1,
+    name: "Same Site Pop-Up",
+    targetHost: window.JS_WISDOM_HOST,
+    targetPath: "/examples/post-message/post-message-popup.html",
+    targetUrl: `${window.JS_WISDOM_HOST}/examples/post-message/post-message-popup.html`,
+    windowLocation: WINDOW_LOCATION.popup,
+  },
+];
--- web-app/src/examples/post-message/post-message.js
+++ web-app/src/examples/post-message/post-message.js
@@ -1,4 +1,5 @@
 import { CODE_SAMPLES } from "./code-samples.js";
+import { MESSAGES, WINDOW_LOCATION } from "./messages.js";
 
 const msgInputElem = document.querySelector("#message-input");
 const sendMsgBtn = document.querySelector("#send-message-button");
@@ -7,6 +8,7 @@ const btnPopupSameOrg = document.querySelector("#btn-popup-same-origin");
 const btnPopupCrossOrg = document.querySelector("#btn-popup-cross-origin");
 const codeSamplesContainer = document.querySelector("#code-samples");
 const iframe = document.querySelector("#post-msg-iframe");
+const sectionMessages = document.querySelector("#section-messages");
 
 iframe.src = `${window.JS_WISDOM_HOST}/examples/post-message/post-message-iframe.html`;
 
@@ -52,3 +54,52 @@ CODE_SAMPLES.forEach((sample) => {
 /*
 Note: Setting target="_blank" on <a> elements implicitly provides the same rel behavior as setting rel="noopener" which does not set window.opener. See browser compatibility for support status.
 */
+
+// MESSAGES SECTION
+MESSAGES.forEach((m) => {
+  const container = document.createElement("div");
+  container.style = "border: 2px solid black;";
+
+  const title = document.createElement("h4");
+  title.textContent = m.name;
+  container.appendChild(title);
+
+  let targetWindow;
+  if (m.windowLocation === WINDOW_LOCATION.popup) {
+    const openPopupBtn = document.createElement("button");
+    openPopupBtn.textContent = "Open Pop-Up";
+    openPopupBtn.addEventListener("click", () => {
+      targetWindow = window.open(m.targetHost + m.targetPath, "_blank");
+    });
+
+    container.appendChild(openPopupBtn);
+  }
+
+  const inputField = document.createElement("input");
+  inputField.type = "text";
+  container.appendChild(inputField);
+
+  const sendMsgBtn = document.createElement("button");
+  sendMsgBtn.textContent = "Send Message";
+  sendMsgBtn.addEventListener("click", () => {
+    targetWindow.postMessage(inputField.value, "*");
+  });
+  container.appendChild(sendMsgBtn);
+
+  const receivedMsg = document.createElement("p");
+  receivedMsg.textContent = "Received Message:";
+  container.appendChild(receivedMsg);
+
+  sectionMessages.appendChild(container);
+
+  window.addEventListener(
+    "message",
+    (event) => {
+      console.log(`Message from ${event.origin} detected.`);
+      if (event.origin === m.targetHost) {
+        receivedMsg.textContent = "Received Message:" + event.data;
+      }
+    },
+    false
+  );
+});
